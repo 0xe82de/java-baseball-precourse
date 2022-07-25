cf3039320cac76588e7f8cfa5bf5beb9297a1648
김경석 <0xe82de@gmail.com>
2022-07-25 22:16:17 +0900

    create: 사용자 플레이어 구현 클래스 생성

---
+++ src/main/java/baseball/domain/numberbaseball/UserPlayer.java
@@ -0,0 +1,10 @@
+package baseball.domain.numberbaseball;
+
+import java.util.List;
+
+public class UserPlayer extends Player {
+
+    public UserPlayer(List<Integer> numbers) {
+        super(numbers);
+    }
+}
