c73bab143898da4186f9f1d27be1767637da4e20
김경석 <0xe82de@gmail.com>
2022-07-25 22:16:26 +0900

    create: 컴퓨터 플레이어 구현 클래스 생성

---
+++ src/main/java/baseball/domain/numberbaseball/Computer.java
@@ -0,0 +1,10 @@
+package baseball.domain.numberbaseball;
+
+import java.util.List;
+
+public class Computer extends Player {
+
+    public Computer(List<Integer> numbers) {
+        super(numbers);
+    }
+}
