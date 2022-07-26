9ac9a4221c7de1f236801b725a50ff7fb283115a
김경석 <0xe82de@gmail.com>
2022-07-26 19:27:27 +0900

    refactor: 사용자 플레이어 클래스 리팩토링

--- src/main/java/baseball/domain/numberbaseball/UserPlayer.java
+++ src/main/java/baseball/domain/numberbaseball/UserPlayer.java
@@ -1,10 +1,4 @@
 package baseball.domain.numberbaseball;
 
-import java.util.List;
-
 public class UserPlayer extends Player {
-
-    public UserPlayer(List<Integer> numbers) {
-        super(numbers);
-    }
 }
