a23213cfe3d90ee08fabde7ed85314e45bc7edb6
김경석 <0xe82de@gmail.com>
2022-09-06 23:40:25 +0900

    refactor: 모델 레이어 추가

---
+++ src/main/java/baseball/domain/numberbaseball/model/Computer.java
@@ -0,0 +1,10 @@
+package baseball.domain.numberbaseball.model;
+
+import java.util.List;
+
+public class Computer extends Player {
+
+    public Computer(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
